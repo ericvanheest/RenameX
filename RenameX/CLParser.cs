38f6d1802b6c09b069dc1fbde5f28c9777913d44
Eric VanHeest <edv_git@vanheest.org>
2023-08-03 09:53:11 -0600

    Initial commit

---
+++ RenameX/CLParser.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RenameX
+{
+    public class CLParser
+    {
+        public string TempFile { get; set; }
+        public string Editor { get; set; }
+        public bool Recursive { get; set; }
+        public bool Directories { get; set; }
+        public bool ShowHelp { get; set; }
+        public string Wildcard { get; set; }
+        public bool Valid { get; set; }
+        public bool Quiet { get; set; }
+
+        public CLParser()
+        {
+            TempFile = "_renx.files";
+            Editor = "notepad.exe";
+            Recursive = false;
+            Directories = false;
+            ShowHelp = false;
+            Wildcard = "*";
+            Valid = false;
+            Quiet = false;
+        }
+
+        enum NextOption { None, Editor, TempFile };
+        enum NextArgs { None, Wildcard };
+
+        public string Version()
+        {
+            return System.Diagnostics.Process.GetCurrentProcess().MainModule.FileVersionInfo.ProductVersion;
+        }
+
+        public void Usage()
+        {
+            Console.WriteLine(String.Format(
+"RenameX, version {0}\n" +
+"\n" +
+"Usage:    RenameX [options] [wildcard]\n" +
+"\n" +
+"Options:  -d         Rename directories instead of files\n" +
+"          -e prog    Use 'prog' to edit files (default: {1})\n" + 
+"          -s         Recurse directories\n" +
+"          -t file    Use 'file' as the temporary file (default: {2})\n" +
+"          -q         Quiet mode (do not print the list of renamed files)\n" +
+"          -?         Show help text\n" +
+"", Version(), Editor, TempFile));
+        }
+
+        public void Parse(string[] args)
+        {
+            NextOption nextOption = NextOption.None;
+            NextArgs nextArg = NextArgs.Wildcard;
+
+            Valid = true;
+
+            foreach (string s in args)
+            {
+                switch (nextOption)
+                {
+                    case NextOption.None:
+                        if (s[0] == '-' || s[0] == '/')
+                        {
+                            for (int i = 1; i < s.Length; i++)
+                            {
+                                switch (s[i])
+                                {
+                                    case 'e':
+                                    case 'E':
+                                        nextOption = NextOption.Editor;
+                                        break;
+                                    case 't':
+                                    case 'T':
+                                        nextOption = NextOption.TempFile;
+                                        break;
+                                    case 's':
+                                    case 'S':
+                                        Recursive = true;
+                                        break;
+                                    case 'd':
+                                    case 'D':
+                                        Directories = true;
+                                        break;
+                                    case 'q':
+                                    case 'Q':
+                                        Quiet = true;
+                                        break;
+                                    case 'h':
+                                    case 'H':
+                                    case '?':
+                                        ShowHelp = true;
+                                        break;
+                                }
+                            }
+                        }
+                        else switch (nextArg)
+                            {
+                                case NextArgs.Wildcard:
+                                    Wildcard = s;
+                                    nextArg = NextArgs.None;
+                                    break;
+                                default:
+                                    Console.WriteLine("Warning: Ignoring extra command line parameters");
+                                    break;
+                            }
+                        break;
+                    case NextOption.Editor:
+                        Editor = s;
+                        nextOption = NextOption.None;
+                        break;
+                    case NextOption.TempFile:
+                        TempFile = s;
+                        nextOption = NextOption.None;
+                        break;
+                }
+            }
+
+            if (ShowHelp)
+            {
+                Usage();
+                Valid = false;
+                return;
+            }
+        }
+    }
+}
---
+++ RenameX/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Diagnostics;
+
+namespace RenameX
+{
+    class Program
+    {
+        static int StringReverse(string x, string y)
+        {
+            return String.Compare(y, x);
+        }
+    
+        static void Main(string[] args)
+        {
+            CLParser parser = new CLParser();
+            parser.Parse(args);
+
+            if (!parser.Valid)
+                return;
+
+            string[] strOrigFiles;
+            if (parser.Directories)
+            {
+                List<string> dirs = new List<string>(Directory.GetDirectories(".", parser.Wildcard, parser.Recursive ? SearchOption.AllDirectories : SearchOption.TopDirectoryOnly));
+                dirs.Sort(StringReverse);
+                strOrigFiles = dirs.ToArray();
+            }
+            else
+                strOrigFiles = Directory.GetFiles(".", parser.Wildcard, parser.Recursive ? SearchOption.AllDirectories : SearchOption.TopDirectoryOnly);
+
+            string[] strOrigFilesNoPath = new string[strOrigFiles.Length];
+            for (int i = 0; i < strOrigFiles.Length; i++)
+            {
+                strOrigFilesNoPath[i] = Path.GetFileName(strOrigFiles[i]);
+            }
+            File.WriteAllLines(parser.TempFile, strOrigFilesNoPath, Encoding.Unicode);
+            Process procEditor = Process.Start(parser.Editor, parser.TempFile);
+            procEditor.WaitForExit();
+            string[] strNewFiles = File.ReadAllLines(parser.TempFile, Encoding.Unicode);
+            //bool bIgnoreLast = false;
+            if (strOrigFilesNoPath.Length != strNewFiles.Length)
+            {
+                if ((strNewFiles.Length == strOrigFilesNoPath.Length + 1) && strNewFiles[strNewFiles.Length - 1] == "")
+                {
+                    // bIgnoreLast = true;
+                }
+                else
+                {
+                    Console.WriteLine("Error: file list lengths differ (original: {0}, new: {1})", strOrigFilesNoPath.Length, strNewFiles.Length);
+                    return;
+                }
+            }
+
+            for (int i = 0; i < strOrigFiles.Length; i++)
+            {
+                string strSource = strOrigFiles[i];
+                string strDest = Path.Combine(Path.GetDirectoryName(strSource), strNewFiles[i]);
+                try
+                {
+                    if (parser.Directories)
+                    {
+                        if (Directory.Exists(strDest))
+                        {
+                            if (!parser.Quiet)
+                                Console.WriteLine("Already exists: {0}", strNewFiles[i]);
+                        }
+                        else if (!Directory.Exists(strSource))
+                        {
+                            Console.WriteLine("Error: Could not find source directory: {0}", strOrigFiles[i]);
+                        }
+                        else
+                        {
+                            Directory.Move(strSource, strDest);
+                            if (!parser.Quiet)
+                                Console.WriteLine("{0} => {1}", strOrigFilesNoPath[i], strNewFiles[i]);
+                        }
+                    }
+                    else
+                    {
+                        if (File.Exists(strDest))
+                        {
+                            if (!parser.Quiet)
+                                Console.WriteLine("Already exists: {0}", strNewFiles[i]);
+                        }
+                        else if (!File.Exists(strSource))
+                        {
+                            Console.WriteLine("Error: Could not find source file: {0}", strOrigFiles[i]);
+                        }
+                        else
+                        {
+                            File.Move(strSource, strDest);
+                            if (!parser.Quiet)
+                                Console.WriteLine("{0} => {1}", strOrigFilesNoPath[i], strNewFiles[i]);
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error moving {0} => {1} : {2}", strOrigFilesNoPath[i], strNewFiles[i], ex.Message);
+                }
+            }
+
+            File.Delete(parser.TempFile);
+        }
+    }
+}
